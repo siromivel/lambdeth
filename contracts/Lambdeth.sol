90a921e3172dfaac6f0c684f457a22cbcbf498b9
Elmo Jones <siromivel@gmail.com>
2019-02-16 11:54:13 -0700

    Assembly implementation of map

--- contracts/Lambdeth.sol
+++ contracts/Lambdeth.sol
@@ -1,41 +1,38 @@
 pragma solidity ^0.5.0;
 
 
-contract Lambdeth {
+library Lambdeth {
+
     function map(address calluh, uint256[] memory arr, bytes4 cb) public returns (uint[] memory) {
+
         uint length = arr.length;
-        uint256[] memory coll = new uint256[](arr.length);
+        uint upper  = (length + 31) / 32;
 
-        for (uint i = 0; i < length; i++) {
-            // coll[i] = caller.call(callbackId, arr[i]);
-            // coll[i] = caller.call(keccak256("cb(uint)"));
-            // uint transformed = caller.call(cb, arr[i]);
+        assembly {
+            let return_array := mload(0x40)
+            mstore(return_array, 0x20)
+            mstore(add(return_array, 32), length)
 
-            uint256 iteratee = arr[i];
+            let lambda := mload(0x40)
+            mstore(lambda, cb)
 
-            assembly {
-                let free_mem := mload(0x40)
-                mstore(free_mem, cb)
-                mstore(add(free_mem, 0x04), iteratee)
+            for { let i := 0 } lt(i, upper) { i := add(i, 1) } {
+                mstore(add(lambda, 0x04), mload(add(return_array, mul(32, add(1, i)))))                
 
                 let result := call(
                     5000,
                     calluh,
                     0,
-                    free_mem,
+                    return_array,
                     0x20,
-                    free_mem,
+                    return_array,
                     0x20
                 )
 
                 let transformed := mload(result)
-                mstore(0x40, add(free_mem, 0x20))
+
+                mstore(0x40, add(transformed, add(0x20, mul(i, 0x20))))
             }
-
         }
-
-        return coll;
     }
 }
-
-// (dc.call(bytes4(keccak256("setA(uint256)")),_val))
