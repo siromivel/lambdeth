acdd05adeaf8866fb070bb3b1f910b97ce8a1ce9
Tara Regan <tarar@kiva.org>
2019-02-16 15:08:52 -0700

    fixing mock contract test

--- test/LambdethTest.js
+++ test/LambdethTest.js
@@ -6,17 +6,16 @@ const zosLib = require('zos-lib')
 const zos = require('zos')
 zosLib.ZWeb3.initialize(web3.currentProvider)
 
-const Lambdeth = artifacts.require('Lambdeth')
+const LambdethMock = artifacts.require('LambdethMock')
 const TestHelper = zos.TestHelper
 
-contract('Lambdeth', function ([_, owner]) {
+contract('LambdethMock', function ([_, owner]) {
   beforeEach(async function () {
     this.project = await TestHelper({from: owner})
   })
 
   it('should transform an array', async function () {
-    console.log(Lambdeth)
-    const proxy = await this.project.createProxy(Lambdeth)
+    const proxy = await this.project.createProxy(LambdethMock)
     const result = await proxy.map([1, 2, 3], function(val) {
       return val * 2
     })
